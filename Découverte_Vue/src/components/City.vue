dcbe86d271a48a6cecd013a36f6fa1a7ba5c66ea
Justinmlnr <juju.vlb38@gmail.com>
2026-05-04 14:50:57 +0200

    Exo 6.2

--- Découverte_Vue/src/components/City.vue
+++ Découverte_Vue/src/components/City.vue
@@ -3,12 +3,13 @@
         <h2>Ville : {{ name }}</h2>
         <p> Météo : {{ weather }}</p>
         <p>Température : {{ temperature }} C</p>
-        <p>Dernière mise à jour : {{ formatedDate }}</p>
+        <p>Dernière mise à jour : {{ formattedDate }}</p>
     </div>
 
 </template>
 <script setup >
 import { computed } from 'vue' // crée une valeur calculée automatiquement à partir d'autre données
+import { format } from 'timeago.js'
 const props = defineProps 
     ({
        name: String,
@@ -16,11 +17,14 @@ const props = defineProps
        temperature: Number,
        updatedAt: Date
     })
-const formatedDate = computed(() => {
-    if (!props.updatedAt) {
-    return "Date inconnue"
-  }
 
-  return new Date(props.updatedAt).toLocaleString()
+const formattedDate = computed(() => {
+  return format(props.updatedAt)
 })
+// const formatedDate = computed(() => {
+//     if (!props.updatedAt) {
+//     return "Date inconnue"
+//   }
+
+//   return new Date(props.updatedAt)
 </script>
