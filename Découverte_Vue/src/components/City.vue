3de39c532d27e3dd1498d0ed4c4702126e7df40d
Justinmlnr <juju.vlb38@gmail.com>
2026-05-04 14:31:01 +0200

    Exo 6.1

--- Découverte_Vue/src/components/City.vue
+++ Découverte_Vue/src/components/City.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
-        <h2>Ville : {{ city.name }}</h2>
-        <p> Météo : {{ city.weather }}</p>
-        <p>Température : {{ city.temperature }} C</p>
+        <h2>Ville : {{ name }}</h2>
+        <p> Météo : {{ weather }}</p>
+        <p>Température : {{ temperature }} C</p>
         <p>Dernière mise à jour : {{ formatedDate }}</p>
     </div>
 
 </template>
 <script setup >
-const city = 
-    {
-       name: "Grenoble",
-       weather: "Pluie forte",
-       temperature: 12.5,
-       updatedAt: new Date()
-    }
-const formatedDate = city.updatedAt.toLocaleString();
+import { computed } from 'vue' // crée une valeur calculée automatiquement à partir d'autre données
+const props = defineProps 
+    ({
+       name: String,
+       weather: String,
+       temperature: Number,
+       updatedAt: Date
+    })
+const formatedDate = computed(() => {
+    if (!props.updatedAt) {
+    return "Date inconnue"
+  }
+
+  return new Date(props.updatedAt).toLocaleString()
+})
 </script>
--- Découverte_Vue/src/views/CitiesList.vue
+++ Découverte_Vue/src/views/CitiesList.vue
@@ -1,11 +1,43 @@
 <template>
     <div>
         <h1> {{ titre }}</h1> 
+
+        <City
+            v-for="city in cities"
+            :key="city.id"
+            :name="city.name"
+            :weather="city.weather"
+            :temperature="city.temperature"
+            :updatedAt="city.updatedAt"
+        />
+
     </div>
 </template>
+
+
 <script setup>
 import { ref } from 'vue'
+import City from "@/components/City.vue";
+
 const titre = ref("Météo - Liste des villes")
+
+const cities = [
+    {
+      id: 1,
+      name: 'Ville 1',
+      weather: 'Ensoleillé',
+      temperature: 22.0,
+      updatedAt: new Date()
+    },
+    {
+      id: 2,
+      name: 'Ville 2',
+      weather: 'Peu nuageux',
+      temperature: 19.5,
+      updatedAt: new Date()
+    }
+]
+
 </script>
 <style>
     
